0439855c361300db1282dbbdfd11500de65c9f25
Erin Mikail Staples <erin@erinmikailstaples.com>
2025-01-17 10:36:15 -0500

    allow credentials

--- backend/src/api.py
+++ backend/src/api.py
@@ -9,8 +9,8 @@ app = FastAPI(title="Jupiter Moons API")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
